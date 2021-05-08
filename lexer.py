3477da4cd70de5409b4b65c881bca0b8e01c72dc
Mohamed-Abdelhamed <43310877+Mohamed-Abdelhamed@users.noreply.github.com>
2021-05-08 21:34:16 +0200

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -8,12 +8,25 @@ class Lexer():
     def _add_tokens(self):
         # Print
         self.lexer.add('PRINT', r'print')
+        self.lexer.add('AND', r'and')
+        self.lexer.add('OR', r'or')
+        self.lexer.add('RingWhen',r'RingWhen')
         # Parenthesis
-        self.lexer.add('OPEN_PAREN', r'\(')
-        self.lexer.add('CLOSE_PAREN', r'\)')
+        self.lexer.add('OPEN_PAREN', r'\{')
+        self.lexer.add('CLOSE_PAREN', r'\}')
+        #Brackets
+        self.lexer.add('OPEN_BRC', r'\(')
+        self.lexer.add('CLOSE_BRC', r'\)')
         # Semi Colon
         self.lexer.add('SEMI_COLON', r'\;')
-        # Operators
+        #Dot
+        self.lexer.add('Dot', r'\.')
+        #Comma
+        self.lexer.add('Comma', r'\,')
+        self.lexer.add('Equals', r'\=')
+        #@
+        self.lexer.add('At', r'\@')
+        # # Operators
         self.lexer.add('SUM', r'\+')
         self.lexer.add('DIFF',r'\-')
         self.lexer.add('DIV',r'\/')
